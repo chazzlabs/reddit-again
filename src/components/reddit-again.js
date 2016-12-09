b20ece7ee54e4fd7a9e3944b91948fda00a4aa6e
Charles Labas <cslabas@gmail.com>
2016-12-09 17:03:37 -0500

    Added body states to reddit-again component to handle transitions between content

--- src/components/reddit-again.js
+++ src/components/reddit-again.js
@@ -10,31 +10,55 @@ export default class RedditAgain extends React.Component {
     constructor() {
         super();
 
+        this.BODY_STATES = { EMPTY: 0, LOADING: 1, THREAD_LIST: 2, THREAD: 3 };
+
         this.state = {
             listings: [],
-            links: []
+            links: [],
+            bodyState: this.BODY_STATES.EMPTY
         };
     }
 
     _onSelect(selectedId) {
+        this.setState({ bodyState: this.BODY_STATES.LOADING });
+
         requestService.getLinks(this.state.listings.find(listing => listing.data.id === selectedId ).data.display_name)
             .then((response) => {
-                this.setState({ links: response.data.children });
+                this.setState({ links: response.data.children, bodyState: this.BODY_STATES.THREAD_LIST });
             });
     }
 
     componentDidMount() {
+        this.setState({ bodyState: this.BODY_STATES.LOADING });
+
         requestService.getSubreddits()
             .then((response) => {
-                this.setState({ listings: response.data.children });
+                this.setState({ listings: response.data.children, bodyState: this.BODY_STATES.EMPTY });
             });
     }
 
     render() {
+        let bodyContent;
+
+        switch (this.state.bodyState) {
+            case this.BODY_STATES.LOADING:
+                bodyContent = <div>Loading...</div>;
+                break;
+            case this.BODY_STATES.THREAD_LIST:
+                bodyContent = <ThreadList links={this.state.links} />;
+                break;
+            case this.BODY_STATES.THREAD:
+                // bodyContent = <Thread />;
+                break;
+            default:
+                bodyContent = <div>Select a subreddit from the dropdown to view its current links.</div>;
+                break;
+        }
+
         return(
             <div>
                 <SubredditChooser listings={this.state.listings} onSelect={this._onSelect.bind(this)}/>
-                <ThreadList links={this.state.links} />
+                {bodyContent}
             </div>
         );
     }
